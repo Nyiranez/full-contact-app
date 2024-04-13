ba56e48805a998492e4d07d1fab2c0397307ee42
Nyiranez <nyiranezalouise93@gmail.com>
2024-04-13 12:10:04 +0200

    same changes on update file

--- src/pages/update.js
+++ src/pages/update.js
@@ -13,13 +13,15 @@ export const Update = () => {
     let { contId } = useParams();
     const navigate = useNavigate();
 
-    const updateContact = (id) => {
-        let updatedContact = {fullName, email, phone, picture}
+    const updateContact = (e, id) => {
+        // e.preventDefault();
+        let updatedContact = { fullName, email, phone, picture }
+
         axios.put(`https://contact-app-server-nxgi.onrender.com/api/v1/contactapp/contact/update?id=${id}`, updatedContact)
             .then((resp) => {
                 console.log(resp.data);
                 alert("successful updated");
-                // navigate(`/details/${id}`); // `/details/${contact._id}`
+                navigate(`/details/${id}`); // `/details/${contact._id}`
             })
             .catch((error) => {
                 console.log(error);
@@ -35,9 +37,9 @@ export const Update = () => {
                 setEmail(contact.email);
                 setPhone(contact.phone);
 
-                if (contact.picture !== undefined) {
-                    setPicture(contact.picture);
-                }
+                // if (contact.picture !== undefined) {
+                //     setPicture(contact.picture);
+                // }
 
             })
             .catch((error) => {
@@ -56,25 +58,28 @@ export const Update = () => {
     return (
         <div className='flex flex-col items-center justify-center'>
             <form>
-
                 <label>Full name</label><br></br>
-                <input type="text" placeholder='Enter your names' value={fullName} className='w-96 h-12 border-2 pl-2'
+                <input type="text" placeholder='Enter your names' value={fullName}
+                    className='w-96 h-12 border-2 pl-2'
                     onChange={e => setFullName(e.target.value)}></input><br></br>
 
-
                 <label>Email</label><br></br>
-                <input type="text" placeholder='Enter your E-mail' value={email} className='w-96 h-12 border-2 pl-2'
-                onChange={e => setEmail(e.target.value)}></input><br></br>
+                <input type="text" placeholder='Enter your E-mail' value={email}
+                    className='w-96 h-12 border-2 pl-2'
+                    onChange={e => setEmail(e.target.value)}></input><br></br>
 
                 <label>Phone</label><br></br>
-                <input type="text" placeholder='Enter your Phone number' value={phone} className='w-96 h-12 border-2 pl-2'
-               onChange={e => setPhone(e.target.value)} ></input><br></br>
+                <input type="text" placeholder='Enter your Phone number' value={phone}
+                    className='w-96 h-12 border-2 pl-2'
+                    onChange={e => setPhone(e.target.value)} ></input><br></br>
+
                 <label for="picture" >Picture</label><br></br>
                 <input type="file" id="picture" name="picture" accept="image/*" className='w-96 h-12 border-2 pl-2'
                 ></input><br></br>
 
 
-                <button className='w-96 h-12 border-2 mt-4 bg-black text-white' onClick={() => updateContact(contId)}>Save Changes</button>
+                <button type="button" className='w-96 h-12 border-2 mt-4 bg-black text-white'
+                    onClick={(e) => updateContact(e, contId)}>Save Changes</button>
             </form>
 
         </div>
